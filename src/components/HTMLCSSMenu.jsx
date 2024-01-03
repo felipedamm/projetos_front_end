77aaa2caf37a9dd482bf971e6526f4d19a3baaa9
felipedamm <86807510+felipedamm@users.noreply.github.com>
2024-01-03 15:54:08 -0300

    configuração de router

--- src/components/HTMLCSSMenu.jsx
+++ src/components/HTMLCSSMenu.jsx
@@ -8,12 +8,12 @@ const HTMLCSSMenu = () => {
         <div id="main-container">
             <div className="html-menu">
                 <h1>Projetos HTML/CSS</h1>
-                <a href="Projetos HTML e CSS/Blog/index.html">Blog</a>
-                <a href="Projetos HTML e CSS/cópia Instagram inicial/index.html">Cópia Pagina inicial Instagram</a>
-                <a href="Projetos HTML e CSS/Formulário Básico/index.html">Formulário básico</a>
-                <a href="Projetos HTML e CSS/Tela inicial empresa/index.html">Tela inicial empresa</a>
-                <a href="Projetos HTML e CSS/Tela inicial Google/index.html">Cópia Google</a>
-                <a href="Projetos HTML e CSS/Tela inicial netflix/index.html">Cópia users Netflix</a>
+                <a href="https://felipedamm.github.io/projetos_html_css/Blog/index.html">Blog</a>
+                <a href="https://felipedamm.github.io/projetos_html_css/cópia Instagram inicial/index.html">Cópia Pagina inicial Instagram</a>
+                <a href="https://felipedamm.github.io/projetos_html_css/Formulário Básico/index.html">Formulário básico</a>
+                <a href="https://felipedamm.github.io/projetos_html_css/Tela inicial empresa/index.html">Tela inicial empresa</a>
+                <a href="https://felipedamm.github.io/projetos_html_css/Tela inicial Google/index.html">Cópia Google</a>
+                <a href="https://felipedamm.github.io/projetos_html_css/Tela inicial netflix/index.html">Cópia users Netflix</a>
             </div>
         </div>
     </div>
